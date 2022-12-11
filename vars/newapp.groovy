55031f0af5cdd41f84eca11675390871b0236664
samantha <etech@gmail.com>
2022-12-11 11:33:20 -0500

    new app codes added
    Signed-off-by: samantha <etech@gmail.com>

---
+++ vars/newapp.groovy
@@ -0,0 +1,23 @@
+def uber(String repoUrl){
+    pipeline{
+        agent any
+        stages{
+            stage('1-build'){
+                steps{
+                    sh 'free -g'
+                }
+            }
+             stage("Checkout Code") {
+               steps {
+                   git branch: 'main',
+                          url: "${repoUrl}"
+               }
+           }
+           stage('closing'){
+            steps{
+                echo "new demo"
+            }
+           }
+        }
+    }
+}
